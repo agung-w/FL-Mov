d5f7725c755c75a7005e394d44ef2650f2e5f16e
agung-w <suronokz3471@yahoo.com>
2022-12-01 23:31:37 +0700

    Fix login page ui

--- lib/presentations/pages/login_page.dart
+++ lib/presentations/pages/login_page.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app/bloc/user_bloc.dart';
 import 'package:movie_app/presentations/widgets/large_button.dart';
+import 'package:movie_app/presentations/widgets/large_input_with_icon.dart';
 import 'package:movie_app/services/auth_services.dart';
 
 class LoginPage extends StatelessWidget {
@@ -16,15 +18,62 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          "Login",
+          style: GoogleFonts.roboto(fontWeight: FontWeight.w400),
+        ),
       ),
       body: Center(
         child: Column(children: [
+          const SizedBox(
+            height: 10,
+          ),
+          const LargeInputWithIcon(
+            icon: Icons.person_2_outlined,
+            hint: "Email",
+            isSecret: false,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const LargeInputWithIcon(
+            icon: Icons.lock_outline,
+            hint: "Password",
+            isSecret: true,
+          ),
+          SizedBox(
+            height: 40,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Row(children: [
+              const Text("Forgot password?"),
+              GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    " reset",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+            ]),
+          ),
           LargeButton(
               function: () {
                 context.read<UserBloc>().add(const UserEvent.signIn(
                     email: "agung@gmail.com", password: "123"));
               },
               title: "Login"),
+          SizedBox(
+            height: 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "OR",
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            ),
+          ),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.8,
             child: ElevatedButton(
@@ -46,6 +95,26 @@ class LoginPage extends StatelessWidget {
                     Text("Google Sign In"),
                   ],
                 )),
+          ),
+          Expanded(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: SizedBox(
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          " Register",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+              ),
+            ),
           ),
         ]),
       ),
